bee92ca144e7e78d236d5aaf2c9206589f56db39
Brent Jackson <jxnblk@github.com>
2015-02-19 11:41:05 -0500

    Use npm scripts

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@
 var React = require('react');
 var data = require('./data');
 
+// Testing npm debug log shit
+var Bumpkit = require('bumpkit');
+var Q = require('q');
+var queryString = require('query-string');
+
 var App = React.createFactory(require('./components/App.jsx'));
 
 React.render(
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,19 @@ var React = require('react');
 var GlobalNav = require('./GlobalNav.jsx');
 var Footer = require('./Footer.jsx');
 
+var Geomicon = require('../../../react-geomicons');
+
 module.exports = React.createClass({
 
   render: function() {
     return (
       <div>
         <GlobalNav {...this.props} />
-        <Footer {...this.props} />
+        <h1>Reactplate</h1>
+        <Geomicon icon="play" className="h3" />
+        <Geomicon icon="pause" className="h1 text-success" />
+        <Geomicon icon="cog" fill="#f00" />
+        <Geomicon icon="heart" />
       </div>
     )
   }
